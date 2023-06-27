7261ed87a407d88c73c63fd93b6651500ad4b0e5
sourajwork <137782608+sourajwork@users.noreply.github.com>
2023-06-27 21:02:27 +0530

    initial commit

---
+++ acer.js
@@ -0,0 +1,51 @@
+
+function changeColor1() {
+     
+    var div = document.getElementById("myDiv1");
+    var currentColor = div.style.backgroundColor;
+  
+    if (currentColor === "rgb(76, 75, 75)") {
+      div.style.backgroundColor = "#666666";
+      document.getElementById("p1").innerHTML = "+";
+    } else {
+      div.style.backgroundColor = "rgb(76, 75, 75)";
+      document.getElementById("p1").innerHTML = "-";
+    }
+
+  }
+  function changeColor2() {
+    var div = document.getElementById("myDiv2");
+    var currentColor = div.style.backgroundColor;
+  
+    if (currentColor === "rgb(76, 75, 75)") {
+      div.style.backgroundColor = "#666666";
+       document.getElementById("p2").innerHTML = "+";
+    } else {
+      div.style.backgroundColor = "rgb(76, 75, 75)";
+       document.getElementById("p2").innerHTML = "-";
+    }
+  }
+  function changeColor3() {
+    var div = document.getElementById("myDiv3");
+    var currentColor = div.style.backgroundColor;
+  
+    if (currentColor === "rgb(76, 75, 75)") {
+      div.style.backgroundColor = "#666666";
+       document.getElementById("p3").innerHTML = "+";
+    } else {
+      div.style.backgroundColor = "rgb(76, 75, 75)";
+       document.getElementById("p3").innerHTML = "-";
+    }
+  }
+  function changeColor4() {
+    var div = document.getElementById("myDiv4");
+    var currentColor = div.style.backgroundColor;
+  
+    if (currentColor === "rgb(76, 75, 75)") {
+      div.style.backgroundColor = "#666666";
+       document.getElementById("p4").innerHTML = "+";
+    } else {
+      div.style.backgroundColor = "rgb(76, 75, 75)";
+       document.getElementById("p4").innerHTML = "-";
+    }
+  }
